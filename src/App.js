8ddc8b5785eb60a43f481a0701b04098c4891ed6
Kilian MAYANS <kilian.mayans@free.fr>
2020-11-16 12:39:50 +0100

    new package + experience filled

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native'
 import $ from 'jquery';
 import './App.scss';
 import Header from './components/Header';
@@ -7,6 +8,7 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import GlobalFont from 'react-native-global-font'
 
 class App extends Component {
   constructor(props) {
@@ -27,7 +29,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setLanguage('en', 'english-flag');
+      let fontName = 'Comfortaa-Regular'
+      GlobalFont.applyGlobal(fontName)
+      this.setLanguage('fr', 'french-flag');
   }
 
   getResumeData(path) {
@@ -44,43 +48,55 @@ class App extends Component {
     });
   }
 
-  // Render of flags
+  // Render
   render() {
     return (
-      <div>
-        <Header data={this.state.resumeData.main} />
-        <div className="col-md-12 mx-auto text-center language">
-            <div
-              onClick={() => this.setLanguage('fr', 'french-flag')}
-              style={{display:'inline'}}>
-              <span
-    className="iconify"
-    data-icon="twemoji-flag-for-flag-france"
-    data-inline="false"
-    style={{fontSize: '50px', cursor: 'pointer'}}
-    id="french-flag"
-    />
-            </div>
-            <div
-              onClick={() => this.setLanguage('en', 'english-flag')}
-              style={{display:'inline', paddingLeft:20}}>
-              <span
-    className="iconify"
-    data-icon="twemoji-flag-for-flag-united-kingdom"
-    data-inline="false"
-    style={{fontSize: '50px', cursor: 'pointer'}}
-    id="english-flag"
-    />
-            </div>
+        <View style={styles.container}>
+          <div>
+            <Header data={this.state.resumeData.main} />
+            <div className="col-md-12 mx-auto text-center language">
+                <div
+                  onClick={() => this.setLanguage('fr', 'french-flag')}
+                  style={{display:'inline'}}>
+                  <span
+        className="iconify"
+        data-icon="twemoji-flag-for-flag-france"
+        data-inline="false"
+        style={{fontSize: '50px', cursor: 'pointer'}}
+        id="french-flag"
+        />
+                </div>
+                <div
+                  onClick={() => this.setLanguage('en', 'english-flag')}
+                  style={{display:'inline', paddingLeft:20}}>
+                  <span
+        className="iconify"
+        data-icon="twemoji-flag-for-flag-united-kingdom"
+        data-inline="false"
+        style={{fontSize: '50px', cursor: 'pointer'}}
+        id="english-flag"
+        />
+                </div>
+              </div>
+            <About data={this.state.resumeData.main} />
+            <Projects data={this.state.resumeData.projects} />
+            <Skills data={this.state.resumeData.resume} />
+            <Experience data={this.state.resumeData.experience} />
+            <Footer data={this.state.resumeData.main} />
           </div>
-        <About data={this.state.resumeData.main} />
-        <Projects data={this.state.resumeData.projects} />
-        <Skills data={this.state.resumeData.resume} />
-        <Experience data={this.state.resumeData.experience} />
-        <Footer data={this.state.resumeData.main} />
-      </div>
+        </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+});
 
 export default App;
